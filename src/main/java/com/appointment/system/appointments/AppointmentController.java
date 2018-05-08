7d9cf0f6131941a88f636b76d5c0b31e459f4d81
Hiram Njogu <bogita155@gmail.com>
2018-05-08 15:15:44 -0500

    Create controllers to handle requests

--- src/main/java/com/appointment/system/appointments/AppointmentController.java
+++ src/main/java/com/appointment/system/appointments/AppointmentController.java
@@ -1,16 +1,41 @@
 package com.appointment.system.appointments;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/appointment")
+@RequestMapping("/appointments")
 public class AppointmentController {
 
-	@GetMapping
-	public String getAppointment(){
-		return "Appointment!";
+	private AppointmentRepository repository;
+
+	public AppointmentController(AppointmentRepository appointmentRepository){
+		this.repository = appointmentRepository;
+	}
+
+	@GetMapping("/{id}")
+	public Appointment getAppointment(@PathVariable(name = "id") Long id){
+		return repository.findOne(id);
+	}
+
+	@GetMapping("/all")
+	public Iterable<Appointment> getAppointments(){
+		return repository.findAll();
+	}
+
+	@PutMapping("/update")
+	public void updateAppointment(@RequestBody Appointment appointment){
+		repository.save(appointment);
+	}
+
+	@PostMapping("/create")
+	public Appointment createAppointment(@RequestBody Appointment appointment){
+		repository.save(appointment);
+		return appointment;
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void deleteAppointment(@PathVariable(name = "id") Long id){
+		repository.delete(id);
 	}
 
 }
--- src/main/java/com/appointment/system/users/UserController.java
+++ src/main/java/com/appointment/system/users/UserController.java
@@ -1,16 +1,41 @@
 package com.appointment.system.users;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-	@GetMapping
-	public String getUser(){
-		return "User!";
+	private UserRepository repository;
+
+	public UserController(UserRepository repository){
+		this.repository = repository;
+	}
+
+	@GetMapping("/{id}")
+	public User getUser(@PathVariable(name = "id") Long id){
+		return repository.findOne(id);
+	}
+
+	@GetMapping("/all")
+	public Iterable<User> getUsers(){
+		return repository.findAll();
+	}
+
+	@PutMapping("/update")
+	public void updateUser(@RequestBody User user){
+		repository.save(user);
+	}
+
+	@PostMapping("/create")
+	public User createUser(@RequestBody User user){
+		repository.save(user);
+		return user;
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void deleteUser(@PathVariable(name = "id") Long id){
+		repository.delete(id);
 	}
 
 }
